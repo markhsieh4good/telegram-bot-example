574f305a2a9f2ab510cba7c3cae43983ed27f68f
mark.hsieh <mark.hsieh@paizaroyal.com.tw>
2023-05-31 08:26:15 +0000

    add how to install python import modules of telegram bot. (ubuntu 22.04: python3.10 only)

---
+++ use_platform/install_python_ub22.04.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+_WHOAMI=`whoami`
+
+cd ~/Downloads/
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 
+python3.10 get-pip.py
+
+if [ ! -e "/usr/bin/pip3.10" ] && [ -e "/home/$_WHOAMI/.local/bin/pip3.10" ]; then
+    sudo ln -s "/home/$_WHOAMI/.local/bin/pip3.10" /usr/bin/pip3.10
+fi
+
+pip3.10 install pipenv
+pipenv install --three python-telegram-bot flask gunicorn requests beautifulsoup4 bs4 chardet decorator idna peewee py
+pip3.10 install python-telegram-bot
+pip3.10 install pytz
+pip3.10 install pyyaml
